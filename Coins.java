227cd3ef31052b2f7a56822f8fe403b750518765
Ivan <ikduong100@student.hbuhsd.edu>
2024-09-22 21:20:39 +0000

    done

--- Coins.java
+++ Coins.java
@@ -2,14 +2,21 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    int coins = 1985;
-    System.out.println(coinConverter(coins));
+    coinConverter(1969);
   }
 
-  public static double coinConverter(int pennies)
+  public static void coinConverter(int pennies)
   {
-    int bills = pennies/100;
-    int quarters = (pennies%100)/25;
+    System.out.println(pennies + " pennies is the same as: ");
+    System.out.println("Dollar bills: " + pennies/100);
+    pennies %= 100;
+    System.out.println("Quarters: " + pennies/25);
+    pennies %= 25;
+    System.out.println("Dimes: " + pennies/10);
+    pennies %= 10;
+    System.out.println("Nickels: " + pennies/5);
+    pennies %= 5;
+    System.out.println("Pennies: " + pennies);
   }
 
 }
